65898d8bf7f38f4a4325c976fb8e11f49bd0ddec
Kanghwan Lee <kanghwan89@gmail.com>
2021-01-18 15:29:51 +0900

    Week 2 Assignment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,10 +1,10 @@
 import { Component } from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
 function RenderDish({dish}) {
-
-    if (dish != null) {
-        return (
+    return (
+        <div className="col-12 col-md-5 m-1">
             <Card>
                 <CardImg width="100%" object src={dish.image} alt={dish.name} />
                 <CardBody>
@@ -12,33 +12,27 @@ function RenderDish({dish}) {
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
-        );
-    } else {
-        return (
-            <div></div>
-        );
-    }
+        </div>
+    );
 }
 
-function RenderComments({dish}) {       
-
-    if (dish != null) {
-
-        const comments = dish.comments.map((comment) => {
-            return (
-                <li>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                </li>                   
-            );
-        });
-
+function RenderComments({comments}) {      
+    if (comments != null) {
         return (
-            <div>
+            <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
-                {comments}
-            </div>                
-        );
+                <ul className="list-unstyled">
+                    {comments.map((comment) => {
+                        return (
+                            <li key={comment.id}>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        );        
     } else {
         return (
             <div></div>  
@@ -47,25 +41,29 @@ function RenderComments({dish}) {
 }
 
 const DishDetail = (props) => {
-
-    console.log('Dishdetail Component render invoked');
-
-    return (
-        <div className="container">            
-            <div className="row">
-                <div className="col-12 col-xs-12 col-sm-12 col-md-5 m-1">                    
-                    <RenderDish dish={props.dish} />
-                </div> 
-                <div className="col-12 col-xs-12 col-sm-12 col-md-5 m-1">                    
-                    <ul className="list-unstyled">                        
-                        <RenderComments dish={props.dish} />
-                    </ul>
-                </div>
-            </div>  
-        </div>          
-    );
+    if (props.dish != null)
+        return (
+            <div className="container"> 
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>           
+                <div className="row">                                        
+                    <RenderDish dish={props.dish} />                 
+                    <RenderComments comments={props.comments} />                        
+                </div>  
+            </div>          
+        );
+    else
+        return (
+            <div></div>
+        );
 };
-
-
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,16 +1,19 @@
-import { Component } from "react";
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
 import { DISHES } from "../shared/dishes";
 import Dishdetail from "./DishdetailComponent";
 
 
 function RenderMenuItem({ dish, onClick }) {
     return (
-        <Card onClick={() => onClick(dish.id)}>                        
-            <CardImg width="100%" object src={dish.image} alt={dish.name} />                        
-            <CardImgOverlay>
-                <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to={`/menu/${dish.id}`} >                        
+                <CardImg width="100%" object src={dish.image} alt={dish.name} />                        
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>
     );
 }
@@ -20,7 +23,7 @@ const Menu = (props) => {
     const menu = props.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-xs-12 col-sm-12 col-md-5 m-1">
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <RenderMenuItem dish={dish} />
             </div>
         );
     });
@@ -28,6 +31,16 @@ const Menu = (props) => {
 
     return (
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
             <div className="row">
                 {menu}
             </div>     
